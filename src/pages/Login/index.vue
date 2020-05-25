8fd671b183f49667c7f06745df888c4bb35cd57f
znh <znh1995@qq.com>
2020-05-25 12:03:34 +0800

    nb

--- src/pages/Login/index.vue
+++ src/pages/Login/index.vue
@@ -44,8 +44,8 @@
       <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
 
       <div class="tips">
-        <span style="margin-right:20px;">username: admin</span>
-        <span> password: 123</span>
+        <!-- <span style="margin-right:20px;">username: brx</span> -->
+        <!-- <span> password: 123456</span> -->
       </div>
 
     </el-form>
